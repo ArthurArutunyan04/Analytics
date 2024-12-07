67c4f49764249fb951d1f5e3651b073e146dfa2f
ArthurArutunyan04 <larturl2004@yandex.ru>
2024-12-07 19:12:49 +0300

    clang-format

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-	string name;
-	cout << "Введите имя: ";
-	cin >> name;
-	cout << "Hello World from @" << name << endl;
-	return 0;
+int
+main()
+{
+  string name;
+  cout << "Введите имя: ";
+  cin >> name;
+  cout << "Hello World from @" << name << endl;
+  return 0;
 }
