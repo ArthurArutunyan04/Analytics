8d93ad9389da99200d7a9986d0ae6c5481356f36
ArthurArutunyan04 <larturl2004@yandex.ru>
2024-12-07 18:53:47 +0300

    add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 
+// Основная функция программы 
 int main(){
 	string name;
+	// Ввод имени пользователя
 	std::cout << "Введите имя: ";
 	std::cin >> name;
+	// Вывод сообщения
 	std::cout << "Hello World from @" << name << std::endl;
 	return 0;
 }
